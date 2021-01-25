b0ebce19181bf7a49643a74611a8eb38a4a22501
[username] <1620987567@qq.com>
2021-01-25 21:57:41 +0800

    asd

--- myblog-rd/config/db.config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'yyz0626',
-    DATABASE: 'myblog'
-}
-
-module.exports = CONFIG
--- myblog-rd/utils/dateTimeUtil.js
+++
@@ -1,3 +0,0 @@
-function formatTime(timestamp, fmt) {
-    //....
-}
